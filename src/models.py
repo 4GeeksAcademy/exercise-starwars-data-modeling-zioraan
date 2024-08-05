6d1bf96d776ed0564b8f144d6bd5285ac5513482
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-08-05 22:48:21 +0000

    Created Base Diagram

--- src/models.py
+++ src/models.py
@@ -13,17 +13,35 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(String(15))
+    skin_color = Column(String(20))
+    birth_year = Column(String(20))
+    eye_color = Column(String(20))
+    gender = Column(String(15))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(50), nullable=False, unique = True)
+    password = Column(String(50), nullable=False)
+    email = Column(String(80), nullable=False)
+    favorites_list = Column(Integer, ForeignKey('favorites.id'), unique=True)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    person_list = Column(Integer, ForeignKey('person.id'))
+    planet_list = Column(Integer, ForeignKey('planet.id'))
+    
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer)
+    gravity = Column(String(40))
+    climate = Column(String(40))
+    terrain = Column(String(40))
+    population = Column(Integer)
 
     def to_dict(self):
         return {}
